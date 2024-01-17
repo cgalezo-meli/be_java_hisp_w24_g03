4d9dd6d75fe4c08790ee0a89277c969b307d2862
cgalezo-meli <camilo.galezocifuentes@mercadolibre.com.co>
2024-01-17 07:18:25 -0500

    [feat] add GET /products/followed/{userId}/list endpoint
    get list of the followed posts of the last two weeks sorted desc by date
    
    add PostService.getUserPost method for getting all posts of a user using user id
    
    add sortDateAsc and sortDateDesc in PostService

--- src/main/java/com/socialmeli/socialmeli/services/PostService.java
+++ src/main/java/com/socialmeli/socialmeli/services/PostService.java
@@ -8,6 +8,7 @@ import com.socialmeli.socialmeli.mapper.Mapper;
 import com.socialmeli.socialmeli.repositories.PostRepositoryImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -51,5 +52,35 @@ public class PostService implements IPostService{
         Post post = mapper.convertDtoToPost(postDto);
         var postList = postRepository.save(post);
         return mapper.convertPostToDto((Post) postList);
+    }
+
+    @Override
+    public List<PostDto> sortDateAsc(List<PostDto> postsDtoList){
+        return postsDtoList.stream().sorted(Comparator.comparing(PostDto::date)).toList();
+    }
+
+    @Override
+    public List<PostDto> sortDateDesc(List<PostDto> postsDtoList){
+        return postsDtoList.stream().sorted(Comparator.comparing(PostDto::date).reversed()).toList();
+    }
+
+    @Override
+    public List<PostDto> getUserPosts(Integer userId){
+        return this.postRepository.findAll().stream().filter(post -> post.getUserId().equals(userId)).map(
+                post -> new PostDto(
+                        post.getUserId(),
+                        post.getDate(),
+                        new ProductDto(
+                                post.getProduct().getProductId(),
+                                post.getProduct().getProductName(),
+                                post.getProduct().getType(),
+                                post.getProduct().getBrand(),
+                                post.getProduct().getColor(),
+                                post.getProduct().getNotes()
+                        ),
+                        post.getCategory(),
+                        post.getPrice()
+                )
+        ).toList();
     }
 }
