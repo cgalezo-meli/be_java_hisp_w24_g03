0cfec8f7cd4335565294e3fa1be9ab1afc5d6b65
vborquezcarm_meli <victoria.borquezcarmona@mercadolibre.cl>
2024-01-26 11:43:40 -0300

    [fix] Changed date so that unit test works at any time and modify expected in findAllTest

--- src/test/java/com/socialmeli/socialmeli/utils/PostUtils.java
+++ src/test/java/com/socialmeli/socialmeli/utils/PostUtils.java
@@ -12,39 +12,39 @@ import java.time.LocalDate;
 public class PostUtils {
     Post postId1 = new Post(1465,
             1,
-            LocalDate.of(2024, 01, 02),
+            LocalDate.now().minusDays(20),
             new Product(1, "Silla Gamer", "Gamer", "Racer", "Red", "Special Edition"),
             100,
             1500.50);
 
     Post postId2 = new Post(1465,
             2,
-            LocalDate.of(2023, 12, 30),
+            LocalDate.now().minusDays(30),
             new Product(1, "Headset RGB Inalámbrico", "Gamer", "Razer", "Green with RGB", "Sin Batería"),
             100,
             1500.50);
 
     Post postId7 = new Post(234,
             7,
-            LocalDate.of(2024, 01, 16),
+            LocalDate.now().minusDays(10),
             new Product(7, "Mochila", "", "Everlast", "Blue", "Permite Notebook"),
             100,
             8000.50);
 
     PostDto postDtoId1 = new PostDto(1465,
-            LocalDate.of(2024, 01, 02),
+            LocalDate.now().minusDays(20),
             new ProductDto(1, "Silla Gamer", "Gamer", "Racer", "Red", "Special Edition"),
             100,
             1500.50);
 
     PostDto postDtoId2 = new PostDto(1465,
-            LocalDate.of(2023, 12, 30),
+            LocalDate.now().minusDays(30),
             new ProductDto(1, "Headset RGB Inalámbrico", "Gamer", "Razer", "Green with RGB", "Sin Batería"),
             100,
             1500.50);
 
     PostDto postDtoId7 = new PostDto(234,
-            LocalDate.of(2024, 01, 16),
+            LocalDate.now().minusDays(10),
             new ProductDto(7, "Mochila", "", "Everlast", "Blue", "Permite Notebook"),
             100,
             8000.50);
